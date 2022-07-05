07db413c9fb53f497d2ad84370c31765fb7ec952
katmart93 <katwlodarczyk93@gmail.com>
2022-07-05 16:02:07 +0200

    form created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div className="App">
       <h1>Guess a Number</h1>
+      <button>Draw a number</button>
+      <div className="game">
+        <div className="users-number">
+          <form>
+            <label>
+              <span>Type your number</span>
+              <input type="number" />
+            </label>
+          </form>
+        </div>
+        <div className="random-number">?</div>
+      </div>
     </div>
   );
 }
