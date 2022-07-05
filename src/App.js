76be9e5ff9279f9d5c5f4887f9a069a58137f44e
katmart93 <katwlodarczyk93@gmail.com>
2022-07-05 18:55:45 +0200

    companring players number and random number

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [randomNumber, setRandomNumber] = useState(null);
   const [playersNumber, setPlayersNumber] = useState(null);
+  const [gameWon, setGameWon] = useState(false);
 
   const drawNumber = () => {
     setRandomNumber(Math.trunc(Math.random() * 10) + 1);
   };
   console.log(randomNumber, playersNumber);
+
+  useEffect(() => {
+    if (randomNumber && playersNumber) {
+      if (randomNumber === playersNumber) {
+        setGameWon(true);
+      }
+    }
+  }, [randomNumber, playersNumber]);
 
   return (
     <div className="App">
@@ -21,13 +30,15 @@ function App() {
               <span>Type a number (1 to 10)</span>
               <input
                 type="text"
-                onChange={(event) => setPlayersNumber(event.target.value)}
+                onChange={(event) =>
+                  setPlayersNumber(parseInt(event.target.value))
+                }
               />
             </label>
           </form>
         </div>
         <div className="random-number">
-          <span>?</span>
+          {gameWon ? <span>{randomNumber}</span> : <div>?</div>}
         </div>
       </div>
     </div>
